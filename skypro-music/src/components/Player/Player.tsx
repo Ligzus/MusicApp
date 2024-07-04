361b8cb9cff4b6a46b09c179d0f92767763ff2af
Ligzus2 <infopop@mail.ru>
2024-07-04 19:24:40 +0300

    + volumeSlider

--- skypro-music/src/components/Player/Player.tsx
+++ skypro-music/src/components/Player/Player.tsx
@@ -1,3 +1,5 @@
+import VolumeSlider from "../VolumeSlider/VolumeSlider";
+
 const Player = () => {
   return (
     <div className="bar">
@@ -64,22 +66,7 @@ const Player = () => {
               </div>
             </div>
           </div>
-          <div className="bar__volume-block volume">
-            <div className="volume__content">
-              <div className="volume__image">
-                <svg className="volume__svg">
-                  <use xlinkHref="img/icon/sprite.svg#icon-volume" />
-                </svg>
-              </div>
-              <div className="volume__progress _btn">
-                <input
-                  className="volume__progress-line _btn"
-                  type="range"
-                  name="range"
-                />
-              </div>
-            </div>
-          </div>
+          <VolumeSlider />          
         </div>
       </div>
     </div>
