efec73b3f8120bcc5201baa482386d11fa756fe9
Ligzus2 <infopop@mail.ru>
2024-08-15 23:11:21 +0300

    npx prettier --write .

--- skypro-music/src/components/CenterBlock/CenterBlock.test.tsx
+++ skypro-music/src/components/CenterBlock/CenterBlock.test.tsx
@@ -1,19 +1,19 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import CenterBlock from './CenterBlock';
-import '@testing-library/jest-dom';
+import { render, screen, waitFor } from "@testing-library/react";
+import CenterBlock from "./CenterBlock";
+import "@testing-library/jest-dom";
 
 // Mock функции и данных
-jest.mock('../../api/tracks', () => ({
-  getTracks: jest.fn(() => Promise.resolve({ data: [] }))
+jest.mock("../../api/tracks", () => ({
+  getTracks: jest.fn(() => Promise.resolve({ data: [] })),
 }));
 
-test('CenterBlock рендерится корректно', async () => {
+test("CenterBlock рендерится корректно", async () => {
   // Рендерим компонент и ожидаем его асинхронное поведение
   const { asFragment } = render(await CenterBlock());
 
   // Используем waitFor для ожидания завершения всех асинхронных операций
   await waitFor(() => {
-    expect(screen.getByText('Треки')).toBeInTheDocument();
+    expect(screen.getByText("Треки")).toBeInTheDocument();
   });
 
   // Проверяем снимок
